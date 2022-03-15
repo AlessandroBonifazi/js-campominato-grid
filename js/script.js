ac1d31b7e1a3c05264e0ec4cd1aeff8032a22a3c
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2022-03-15 16:05:49 +0100

    Init and style

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Debug
+console.log('JS OK!');
+
+// creare una griglia in cui ogni cella contiene un numero tra
+// quelli compresi in un range compreso tra 1 e 100
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+
+
